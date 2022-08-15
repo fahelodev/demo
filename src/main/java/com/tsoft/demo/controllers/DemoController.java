3b3ce3e491c6f82a7fbb84e79b259f1a7911e6e8
faheloDev <fahelo.dev@gmail.com>
2022-08-14 20:03:00 -0400

    rest demo ok

--- src/main/java/com/tsoft/demo/controllers/DemoController.java
+++ src/main/java/com/tsoft/demo/controllers/DemoController.java
@@ -3,11 +3,15 @@ package com.tsoft.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class DemoController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/Demo")
+    public HashMap<String, String> index() {
+        HashMap<String, String> status = new HashMap<>();
+        status.put("status", "ok");
+        return status;
     }
 }
